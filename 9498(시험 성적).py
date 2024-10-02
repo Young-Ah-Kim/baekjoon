78ca952dc65b9ebfdeb595325c4e0ac0383f5a76
YA.Kim <170071016+Young-Ah-Kim@users.noreply.github.com>
2024-10-02 23:52:46 +0900

    Create 9498(시험 성적).py

---
+++ 9498(시험 성적).py
@@ -0,0 +1,11 @@
+grade = input()
+if 90 <= int(grade) <= 100:
+    print("A")
+elif 80 <= int(grade) <= 89:
+    print("B")
+elif 70 <= int(grade) <= 79:
+    print("C")
+elif 60 <= int(grade) <= 69:
+    print("D")
+else:
+    print("F")
